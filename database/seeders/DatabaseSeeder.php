9eddb1df2cbc55596149361924f8c08660a98e14
FarhanSuryadicka <farhansuryadika02@gmail.com>
2026-04-16 13:46:55 +0700

    Add API routes, update user seeder, and configure Sanctum

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'Alip maulana',
-            'email' => 'muhamadalipmaulana3@gmail.com',
-            'password' => Hash::make('Alip210725_'),
-            'role' => 'admin',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'muhamadalipmaulana3@gmail.com'],
+            [
+                'name' => 'Alip maulana',
+                'email' => 'muhamadalipmaulana3@gmail.com',
+                'password' => Hash::make('Alip210725_'),
+                'role' => 'admin',
+            ]
+        );
+        
+        User::updateOrCreate(
+            ['email' => 'admin@mail.com'],
+            [
+                'name' => 'Admin',
+                'email' => 'admin@mail.com',
+                'password' => Hash::make('admin'),
+                'role' => 'admin',
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'mitra@mail.com'],
+            [
+                'name' => 'Mitra',
+                'email' => 'mitra@mail.com',
+                'password' => Hash::make('mitra'),
+                'role' => 'mitra',
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'pelamar@mail.com'],
+            [
+                'name' => 'Pelamar',
+                'email' => 'pelamar@mail.com',
+                'password' => Hash::make('pelamar'),
+                'role' => 'pelamar',
+            ]
+        );
     }
 }
